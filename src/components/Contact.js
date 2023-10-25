d0d7e19974582f33af73e4ad4aa4ab22670d3679
harisai09 <141613942+harisai09@users.noreply.github.com>
2023-10-25 23:30:54 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,17 +11,12 @@ const Contact = () => {
       </p>
       <ul>
         <li>
-          <strong style={labelStyle}>Email:</strong> <a href="mailto:ishanvikotha808@gmail.com">Email</a>
+          <strong style={labelStyle}>Email:</strong> <a href="mailto:veerahari373@gmail.com">Email</a>
         </li>
-        <li>
-          <strong style={labelStyle}>LinkedIn:</strong>{' '}
-          <a href="https://www.linkedin.com/in/ishanvikotha" target="_blank" rel="noopener noreferrer">
-            Linkedin
-          </a>
-        </li>
+        
         <li>
           <strong style={labelStyle}>GitHub:</strong>{' '}
-          <a href="https://github.com/Ishanvi26" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/harisai09" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
         </li>
