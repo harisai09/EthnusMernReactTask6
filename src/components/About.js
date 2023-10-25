110a3b26eccf103d30f675f397a98ebf6be8e69f
harisai09 <141613942+harisai09@users.noreply.github.com>
2023-10-25 23:29:23 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section>
       <h2>About Me</h2>
       <p>
-      Hi, I'm Ishanvi Kotha. I'm a full-stack web developer. I specialize in front-end development, 
+      Hi, I'm Veera Hari Sai. I'm a full-stack web developer. I specialize in front-end development, 
       where I create user-friendly interfaces and engaging user experiences. I'm proficient in JavaScript, 
       React, and Node.js. I'm passionate about creating efficient, responsive web applications. My goal 
       as a developer is to keep learning and growing in this ever-evolving field while contributing to 
